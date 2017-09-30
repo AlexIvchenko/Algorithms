2d20f81b2f282e7d691f5b3e7cb597e00ed8dd47
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2017-09-30 17:18:20 +0300

    move MergeSort in special file

--- src/main/kotlin/com/shurikat/algorithms/sort/Sort.kt
+++ src/main/kotlin/com/shurikat/algorithms/sort/Sort.kt
@@ -1,7 +1,5 @@
 package com.shurikat.algorithms.sort
 
-import com.shurikat.types.Measurable
-
 /**
  * @author Alex Ivchenko
  */
@@ -37,40 +35,6 @@ object SelectionSort : Sorter {
             val t = a[j]
             a[j] = a[minI]
             a[minI] = t
-        }
-    }
-}
-
-object MergeSort : Sorter {
-    override fun <T : Comparable<T>> sort(a: Array<T>) {
-        mergeSort(a, 0, a.size - 1)
-    }
-
-    private fun <T : Comparable<T>> mergeSort(a: Array<T>, p: Int, r: Int) {
-        if (p >= r) {
-            return
-        }
-        val q = (p + r) / 2
-        mergeSort(a, p, q)
-        mergeSort(a, q + 1, r)
-        merge(a, p, q, r)
-    }
-
-    private fun <T : Comparable<T>> merge(a: Array<T>, p: Int, q: Int, r: Int) {
-        val n1 = q - p + 1
-        val n2 = r - q
-        val left = List(n1 + 1, { i ->
-            if (i < n1) Measurable.of(a[p + i])
-            else Measurable.posInfinity() // last element is fictive
-        })
-        val right = List(n2 + 1, { i ->
-            if (i < n2) Measurable.of(a[q + 1 + i])
-            else Measurable.posInfinity() // last element is fictive
-        })
-        var i = 0
-        var j = 0
-        for (k in p..r) {
-            a[k] = if (left[i] < right[j]) left[i++].value!! else right[j++].value!!
         }
     }
 }
